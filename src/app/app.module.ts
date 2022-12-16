ce27bf5a5f0c674a0c1c21785538e76572355b06
riveror <richardrivero@upb.edu>
2022-12-16 14:31:27 -0400

    arreglos parte 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MatInputModule } from '@angular/material/input';
 import {MatTableModule} from '@angular/material/table';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatRadioModule} from '@angular/material/radio';
 
 const rutas:Routes = [
   {path: '', component: InicioComponent, pathMatch: 'full'},
@@ -51,7 +52,8 @@ const rutas:Routes = [
     ReactiveFormsModule,
     MatTableModule,
     MatTooltipModule,
-    MatDividerModule
+    MatDividerModule,
+    MatRadioModule
   ],
   declarations: [
     AppComponent,
--- src/app/tabs/calculadora/calculadora.component.ts
+++ src/app/tabs/calculadora/calculadora.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import { MyErrorStateMatcher } from '../conversiones/conversiones.component';
+
 
 @Component({
   selector: 'app-calculadora',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent implements OnInit {
+  ip: number[] = [192, 168, 10, 0];
+  mk: number[] = [255, 255, 255, 0];
+  rd: number[] = [0, 0, 0, 0];
+  ht: number[] = [0, 0, 0, 0];
+  bt: number[] = [0, 0, 0, 0];
+
+  ipb: number[][] = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+  mkb: number[][] = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+  rdb: number[][] = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+  htb: number[][] = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+  btb: number[][] = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+
+  maskBase: number = 24;
+
+  seg1Control = new FormControl('', [Validators.pattern('[0-9]*')]);
+  seg2Control = new FormControl('', [Validators.pattern('[0-9]*')]);
+  seg3Control = new FormControl('', [Validators.pattern('[0-9]*')]);
+  seg4Control = new FormControl('', [Validators.pattern('[0-9]*')]);
+  maskControl = new FormControl('', [Validators.pattern('[0-9]*')]);
+  matcher = new MyErrorStateMatcher();
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
